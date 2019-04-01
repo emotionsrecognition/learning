bebfb17afed549f580c787ce62a3b3a164a90b96
toshev.is <toshev.is@skbkontur.ru>
2019-04-01 11:24:15 +0500

    Updated

--- features.py
+++ features.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import os
+import shutil
 import subprocess
+from pathlib import Path
+from typing import Iterable
+
 import datasets
 
 
@@ -17,30 +21,73 @@ EMOBASE = '{0} -C {1}'.format(smileextract_path, os.path.join(config_path, 'emob
 EMOLARGE = '{0} -C {1}'.format(smileextract_path, os.path.join(config_path, 'emo_large.conf'))
 EMO2010 = '{0} -C {1}'.format(smileextract_path, os.path.join(config_path, 'emobase2010.conf'))
 
-    
-def get_features(info_list, config):
-    open('awkward', 'w+').close()
-    base_command = config + ' -I {0} -O {1} -nologfile'
-    command = []
-    command_len = 0
-    for p in info_list:
-        temp_command = base_command.format(p.file_path, 'awkward')
-        if os.name == 'nt':
-            command_len += len(temp_command)
-            if command_len > 8000:
-                final_command = COM_SEP.join(command)
-                subprocess.check_output(final_command, shell=True, stderr=subprocess.STDOUT)
-                command = []
-                command_len = len(temp_command)
-        command.append(temp_command)
-    final_command = COM_SEP.join(command)
-    subprocess.check_output(final_command, shell=True, stderr=subprocess.STDOUT)
-    features = []
-    with open('awkward') as f:
-        for s in f:
-            features.append(list(map(float, s.split(',')[1: -1])))
-    open('awkward', 'w+').close()
-    return features
+AGGREGATES_PATH = 'data_csv/aggregates.csv'
+AGGREGATES_COPY_PATH = 'data_csv/aggregates_copy.csv'
+
+
+def get_output_path(input_path: str) -> str:
+    parts = list(Path(input_path).parts)
+    parts[parts.index('data')] = 'data_csv'
+    return os.path.join(*parts) + '.csv'
+
+
+def get_features(info_list: Iterable[datasets.FileInfo], config_path: str) -> None:
+    base_command = config_path + ' -I "{input}" -O "{output}" -A "{aggregates}" -N {line_name} -nologfile'
+    for info in info_list:
+        command = base_command.format(
+            aggregates=AGGREGATES_PATH, input=info.file_path,
+            output=get_output_path(info.file_path), line_name=info.file_path
+        )
+        subprocess.check_output(command, shell=True)
+
+
+def mirror_dir_tree(base_path: str) -> None:
+    def ignore(dir, files):
+        return [f for f in files if os.path.isfile(os.path.join(dir, f))]
+
+    shutil.copytree(base_path, f'{base_path}_csv', ignore=ignore)
+
+
+def process_aggreagtes() -> None:
+    sets = {
+        f.file_path: f
+        for ds in (datasets.get_emovo(), datasets.get_ravdess(), datasets.get_cafe(), datasets.get_berlin(), datasets.get_tess())
+        for f in ds
+    }
+
+    with open(AGGREGATES_PATH) as f, open(AGGREGATES_COPY_PATH, 'w+') as new:
+        it = iter(f)
+        next(f)
+
+        for line in it:
+            index = line.index(';')
+            path = line[:index].strip("'")
+            if not path.endswith('wav'):
+                continue
+
+            info = sets[path]
+            new_line = (
+                # sry for this
+                "'" + path[len(r'C:\Projects\Emotions\learning\\') - 1:] +
+                f"';{str(info.emotion.value)};{info.actor};{info.gender.value};{info.intensity.value};{info.age.value}" +
+                line[index:]
+            )
+
+            new.write(new_line)
+
 
 if __name__ == '__main__':
+    try:
+        mirror_dir_tree('data')
+    except Exception:
+        pass
+
+    print(get_features(datasets.get_emovo(), EMO2010))
     print(get_features(datasets.get_ravdess(), EMO2010))
+    print(get_features(datasets.get_cafe(), EMO2010))
+    print(get_features(datasets.get_berlin(), EMO2010))
+    print(get_features(datasets.get_tess(), EMO2010))
+
+    process_aggreagtes()
+
+
